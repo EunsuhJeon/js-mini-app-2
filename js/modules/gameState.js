6fc516451a3f14a35596f4699cbd815d247fabbf
Santiagoo <santiagominotta2913@hotmail.com>
2026-01-04 19:26:21 -0800

    Add initializeRound function in gameState.js

--- js/modules/gameState.js
+++ js/modules/gameState.js
@@ -1,7 +1,9 @@
 import fishData from './fish.js';
 import { getCaughtFish } from './playerState.js';
 
-// Initial game state
+// =================================
+// INITIAL GAME STATES
+// =================================
 const gameState = {
     //------------------------------
     // Info about the current round
@@ -35,6 +37,36 @@ const gameState = {
     // Info for every round
     targetDepth: null, // Right depth to catch the current fish
     squenceKeys: [], // Keys to type during the typing phase
-    requieredClicks: 0, // Clicks required to reel in the fish
+    requieredClicks: 0 // Clicks required to reel in the fish
 };
 
+// =================================
+// MAIN GAME FUNCTIONS
+// =================================
+
+// ------------------------------
+// Initialize a new round
+export function initializeRound() {
+    console.log(`Initializing round ${gameState.currentRound}...`);
+
+    // ------------------------------
+    // Restart temporal states
+    gameState.currentPhase = 'idle';
+    gameState.isCasting = false;
+    gameState.isFishHooked = false;
+    gameState.currentBait = null;
+    gameState.currentDepth = null;
+    gameState.phaseScore = 0;
+    gameState.reelTime = 0;
+    gameState.keyTime = 0;
+
+    // ------------------------------
+    // Set difficulty based on the fish rarity
+    if (gameState.currentFish) {
+        const rarity = gameState.currentFish.rarity;
+        gameState.hookZoneSize = rarity === 'common' ? 40 : rarity === 'rare' ? 30 : 20;
+        gameState.requieredClicks = rarity === 'common' ? 10 : rarity === 'rare' ? 15 : 20;
+    }
+
+    return gameState;
+}
